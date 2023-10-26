1a40d6c26bc34595d84cc307158ab1c14228d55b
River James <river.l.james@students.ittralee.ie>
2023-10-26 22:12:41 +0100

    feature/update item component fixed and popup dialog appearing correctly.

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,25 @@
   <v-app>
     <v-main>
       <CreateItemComponent @create-item="createItem" />
+      <EditItemComponent
+        ref="editItemComponent"
+        v-if="editedItem"
+        :item="editedItem"
+        @save-item="editItem"
+        @close-edit="editedItem = null"
+      />
 
       <v-list>
         <v-list-item-group v-if="items.length > 0">
           <v-list-item v-for="item in items" :key="item._id">
             <v-list-item-content>
               <v-list-item-title>{{ item.name }}</v-list-item-title>
-              <v-list-item-subtitle>{{ item.description }}</v-list-item-subtitle>
+              <v-list-item-subtitle>{{
+                item.description
+              }}</v-list-item-subtitle>
             </v-list-item-content>
             <v-list-item-action>
-              <v-btn icon @click="editItem(item)">
+              <v-btn icon @click="startEditing(item)">
                 <v-icon>mdi-pencil</v-icon>
               </v-btn>
               <v-btn icon @click="deleteItem(item._id)">
@@ -32,17 +41,20 @@
 <script>
 import PouchDB from "pouchdb-browser";
 import CreateItemComponent from "./components/CreateItemComponent.vue";
-import { v4 as uuidv4 } from 'uuid';
+import EditItemComponent from "./components/EditItemComponent.vue";
+import { v4 as uuidv4 } from "uuid";
 
 export default {
   components: {
     CreateItemComponent,
+    EditItemComponent,
   },
   data() {
     return {
       db: null,
       remoteCouch: false,
       items: [],
+      editedItem: null,
     };
   },
   created() {
@@ -59,17 +71,17 @@ export default {
     },
     syncError(err) {
       console.error("Database synchronization error:", err);
-      this.notify('Database synchronization error', 'error');
+      this.notify("Database synchronization error", "error");
     },
     async createItem(item) {
       try {
         item._id = uuidv4();
         await this.db.put(item);
         this.fetchItems();
-        this.notify('Item created successfully', 'success');
+        this.notify("Item created successfully", "success");
       } catch (err) {
         console.error("Error creating item:", err);
-        this.notify('Error creating item', 'error');
+        this.notify("Error creating item", "error");
       }
     },
     async fetchItems() {
@@ -80,35 +92,48 @@ export default {
         console.error("Error fetching items:", err);
       }
     },
-    async editItem(item) {
-      try {
-        await this.db.put(item);
-        this.fetchItems();
-        this.notify('Item updated successfully!', 'success');
-      } catch (err) {
-        console.error("Error updating item:", err);
-        this.notify('Error updating item', 'error');
-      }
+    startEditing(item) {
+      this.editedItem = { ...item };
+  this.$nextTick(() => {
+    if (this.$refs.editItemComponent) {
+      this.$refs.editItemComponent.dialog = true;
+    } else {
+      console.error("EditItemComponent is not mounted or ref is not set correctly");
+    }
+  });
+    },
+    editItem(item) {
+      this.db
+        .put(item)
+        .then(() => {
+          this.fetchItems();
+          this.editedItem = null;
+          this.notify("Item updated successfully!", "success");
+        })
+        .catch((err) => {
+          console.error("Error updating item:", err);
+          this.notify("Error updating item", "error");
+        });
     },
     async deleteItem(id) {
       try {
         const doc = await this.db.get(id);
         await this.db.remove(doc);
         this.fetchItems();
-        this.notify('Item deleted successfully!', 'success');
+        this.notify("Item deleted successfully!", "success");
       } catch (err) {
         console.error("Error deleting item:", err);
-        this.notify('Error deleting item', 'error');
+        this.notify("Error deleting item", "error");
       }
     },
     notify(message, type) {
-      this.$notify({ 
-        group: 'main', 
+      this.$notify({
+        group: "main",
         title: type.charAt(0).toUpperCase() + type.slice(1),
         text: message,
-        type: type
+        type: type,
       });
-    }
+    },
   },
 };
 </script>
--- src/components/EditItemComponent.vue
+++ src/components/EditItemComponent.vue
@@ -6,11 +6,25 @@
       <v-card>
         <v-card-title>Edit Item</v-card-title>
         <v-card-text>
-          <v-text-field v-model="editedItem.name" label="Item Name"></v-text-field>
-          <v-text-field v-model="editedItem.description" label="Description"></v-text-field>
+          <v-text-field 
+            v-model="editedItem.name" 
+            label="Item Name" 
+            :error-messages="nameErrors" 
+            @input="validateName"
+            required
+          ></v-text-field>
+          <v-text-field 
+            v-model="editedItem.description" 
+            label="Description" 
+            :error-messages="descriptionErrors" 
+            @input="validateDescription"
+            required
+          ></v-text-field>
         </v-card-text>
         <v-card-actions>
-          <v-btn @click="saveItem">Save</v-btn>
+          <v-btn text @click="closeDialog">Cancel</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn :loading="saving" :disabled="saving" @click="saveItem">Save</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -19,26 +33,72 @@
   <script>
   export default {
     props: {
-      item: Object,
+      item: {
+        type: Object,
+        default: () => ({})
+      }
     },
     data() {
       return {
         dialog: false,
-        editedItem: this.item,
+        editedItem: {},
+        saving: false,
+        nameErrors: [],
+        descriptionErrors: []
       };
     },
     watch: {
-      item: "setItem",
+      item: {
+        immediate: true,
+        handler: "initEditedItem"
+      }
     },
     methods: {
-      setItem() {
+      initEditedItem() {
         this.editedItem = { ...this.item };
       },
-      async saveItem() {
-        await this.$emit('save-item', this.editedItem);
-        this.dialog = false;
+      validateName() {
+        this.nameErrors = [];
+        if (!this.editedItem.name) {
+          this.nameErrors.push("Name is required");
+        }
       },
-    },
+      validateDescription() {
+        this.descriptionErrors = [];
+        if (!this.editedItem.description) {
+          this.descriptionErrors.push("Description is required");
+        }
+      },
+      async saveItem() {
+        if (this.validate()) {
+          this.saving = true;
+          try {
+            await this.$emit('save-item', this.editedItem);
+            await this.$nextTick();
+            this.dialog = false;
+          } catch (error) {
+            console.error("Error saving item:", error);
+            this.$notify({ 
+              group: 'main', 
+              title: 'Error',
+              text: 'Failed to save item. Please try again.',
+              type: 'error'
+            });
+          } finally {
+            this.saving = false;
+          }
+        }
+      },
+      validate() {
+        this.validateName();
+        this.validateDescription();
+        return this.nameErrors.length === 0 && this.descriptionErrors.length === 0;
+      },
+      closeDialog() {
+        this.dialog = false;
+        this.$emit('close');
+      }
+    }
   };
   </script>
   
