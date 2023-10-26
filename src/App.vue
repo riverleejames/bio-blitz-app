2efc274ffe7f57c23eb5975da49756454efc5aef
River James <river.l.james@students.ittralee.ie>
2023-10-26 21:33:56 +0100

    feature/Create, Read, Delete working

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,103 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+      <CreateItemComponent @create-item="createItem" />
+
+      <v-list>
+        <v-list-item-group v-if="items.length > 0">
+          <v-list-item v-for="item in items" :key="item._id">
+            <v-list-item-content>
+              <v-list-item-title>{{ item.name }}</v-list-item-title>
+              <v-list-item-subtitle>{{
+                item.description
+              }}</v-list-item-subtitle>
+            </v-list-item-content>
+            <v-list-item-action>
+              <v-btn icon @click="editItem(item)">
+                <v-icon>mdi-pencil</v-icon>
+              </v-btn>
+              <v-btn icon @click="deleteItem(item._id)">
+                <v-icon>mdi-delete</v-icon>
+              </v-btn>
+            </v-list-item-action>
+          </v-list-item>
+        </v-list-item-group>
+        <v-list-item v-else>
+          <v-list-item-content>No items found</v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PouchDB from "pouchdb-browser";
+import CreateItemComponent from "./components/CreateItemComponent.vue";
 
 export default {
-  name: 'App',
-
   components: {
-    HelloWorld,
+    CreateItemComponent,
   },
+  data() {
+  return {
+    db: null,
+    remoteCouch: false,
+    items: [],
+    // Other data properties
+  };
+},
 
-  data: () => ({
-    //
-  }),
-}
+  created() {
+  this.db = new PouchDB('LocalBioBlitz');
+  this.remoteCouch = 'http://admin:ca7uwaxa@localhost:5984/js_test';
+  this.sync();
+  this.fetchItems();
+},
+  methods: {
+    sync() {
+      const opts = { live: true };
+      this.db.replicate.to(this.remoteCouch, opts, this.syncError);
+      this.db.replicate.from(this.remoteCouch, opts, this.syncError);
+    },
+    syncError(err) {
+      console.error("Database synchronization error:", err);
+    },
+    async createItem(item) {
+      try {
+        item._id = new Date().toISOString();
+        const response = await this.db.put(item);
+        console.log("Item created successfully:", response);
+      } catch (err) {
+        console.error("Error creating item:", err);
+      }
+    },
+    async fetchItems() {
+      try {
+        const result = await this.db.allDocs({ include_docs: true });
+        this.items = result.rows.map((row) => row.doc);
+      } catch (err) {
+        console.error("Error fetching items:", err);
+      }
+    },
+    async editItem(item) {
+      try {
+        const updatedItem = await this.db.put(item);
+        this.fetchItems();
+        return updatedItem;
+      } catch (err) {
+        console.error("Error updating item:", err);
+      }
+    },
+
+    async deleteItem(id) {
+      try {
+        const doc = await this.db.get(id);
+        await this.db.remove(doc);
+        this.fetchItems();
+      } catch (err) {
+        console.error("Error deleting item:", err);
+      }
+    },
+  },
+};
 </script>
---
+++ src/components/CreateItemComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-card>
+      <v-card-title>Create New Item</v-card-title>
+      <v-card-text>
+        <v-text-field v-model="newItem.name" label="Item Name"></v-text-field>
+        <v-text-field v-model="newItem.description" label="Description"></v-text-field>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn @click="createItem">Create</v-btn>
+      </v-card-actions>
+    </v-card>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        newItem: {
+          name: '',
+          description: '',
+        },
+      };
+    },
+    methods: {
+      createItem() {
+        this.$emit('create-item', this.newItem);
+        this.newItem = { name: '', description: '' };
+      },
+    },
+  };
+  </script>
+  
